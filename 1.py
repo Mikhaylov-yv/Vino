937dd46b03d5ea28670a9f951b93e2babf7ffaa1
dezmond22 <dezmond22@yandex.ru>
2018-10-26 17:57:07 +0300

    1

--- 1.py
+++ 1.py
@@ -10,9 +10,13 @@ import matplotlib.pyplot as plt
 FileName1 = 'winequality-red.csv'
 myFile1 = open(FileName1, 'r',newline='')
 df = pd.read_csv(FileName1, sep = ';')
+print(df['quality'].value_counts())
 
 print(df.head())
 print(df.info())
-import seaborn as sns
-sns.pairplot(df, hue='residual sugar')
+df['quality'].value_counts().plot(kind='bar', label='quality')
+plt.show()
+#Строим корреляцию
+corr_matrix = df.corr()
+sns.heatmap(corr_matrix)
 plt.show()
